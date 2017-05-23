32bedcc52bf19ce4437bdb8597a0235104c40667
Mork Pin <morkpin@gmail.com>
2017-05-23 17:30:39 +0700

    startup

--- app/src/main/java/kh/edu/app/myproject/lc_luncher/StartupActivity.java
+++ app/src/main/java/kh/edu/app/myproject/lc_luncher/StartupActivity.java
@@ -1,7 +1,11 @@
 package kh.edu.app.myproject.lc_luncher;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 public class StartupActivity extends AppCompatActivity {
 
@@ -9,7 +13,16 @@ public class StartupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startup);
-
-        
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                startHomeActivity();
+                finish();
+            }
+        },2000);
+    }
+    private  void  startHomeActivity(){
+        Intent HomeIntent = new Intent(this,HomeActivity.class);
+        startActivity(HomeIntent);
     }
 }

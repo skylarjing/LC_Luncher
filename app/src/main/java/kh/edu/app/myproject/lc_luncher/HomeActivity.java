9e58505e1606ebf3163916be0073e144d48e1225
Mork Pin <morkpin@gmail.com>
2017-05-23 17:40:39 +0700

    HomeActivity

--- app/src/main/java/kh/edu/app/myproject/lc_luncher/HomeActivity.java
+++ app/src/main/java/kh/edu/app/myproject/lc_luncher/HomeActivity.java
@@ -2,6 +2,8 @@ package kh.edu.app.myproject.lc_luncher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Recycler View
+        RecyclerView rclCourses = (RecyclerView) findViewById(R.id.rclview_food);
+
+        //Layout Manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        rclCourses.setLayoutManager(layoutManager);
+
+
+
+        // Adapter
+
     }
+
 }

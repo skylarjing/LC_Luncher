bfdbb0e4cda95095a40f69c379e9b1b297ed73d5
Mork Pin <morkpin@gmail.com>
2017-06-15 17:56:33 +0700

    viewholderlayout

--- app/src/main/java/kh/edu/app/myproject/lc_luncher/HomeActivity.java
+++ app/src/main/java/kh/edu/app/myproject/lc_luncher/HomeActivity.java
@@ -6,15 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import kh.edu.app.myproject.lc_luncher.DataModel.HomeText;
 
 public class HomeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,79 +35,34 @@ public class HomeActivity extends AppCompatActivity {
         //Layout Manager
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         rclhometext.setLayoutManager(layoutManager);
+        lordHomeTextFromServer();
 
-        HomeText[] hometext= loadhometext();
+    }
+    private void lordHomeTextFromServer() {
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        StringRequest request=new StringRequest("url", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+    }
+
 
 
         // Adapter
-        HomeAdapter adapter = new HomeAdapter();
-        adapter.hometext= hometext;
-        rclhometext.setAdapter(adapter);
+
 
     }
 
-    private HomeText[] loadhometext(){
-        HomeText text1=new HomeText();
-
-        text1.txtFood="ginger";
-        text1.txtPrice="R5,000";
-
-        HomeText text2=new HomeText();
-        text2.txtFood="brocoli";
-        text2.txtPrice="R7,000";
-        return new HomeText[]{text1, text2};
-    }
-    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder>{
-        HomeText[] hometext;
-
-        @Override
-        public HomeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-            View homeViewHolderLayout= LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_home,parent,false);
-            return new HomeViewHolder(homeViewHolderLayout);
-            }
-
-        @Override
-        public void onBindViewHolder(HomeViewHolder holder, int position) {
-            HomeText homeText =hometext[position];
-            //holder.imgfood.setImageResource(hometext[position].getImage)
-            holder.txtfood.setText(homeText.txtFood);
-            holder.txtprice.setText(homeText.txtPrice);
-        }
-
-        @Override
-        public int getItemCount() {
-            return hometext.length;
-        }
-
-        class HomeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-            ImageView imgfood;
-            ImageView imgblack;
-            TextView txtfood;
-            TextView txtprice;
-
-            public HomeViewHolder(View itemView) {
-                super(itemView);
-                imgfood=(ImageView) itemView.findViewById(R.id.img_food);
-                imgblack=(ImageView) itemView.findViewById(R.id.img_black);
-                txtfood=(TextView) itemView.findViewById(R.id.txt_food);
-                txtprice=(TextView) itemView.findViewById(R.id.txt_price);
-                itemView.setOnClickListener(this);
-            }
-
-            @Override
-            public void onClick(View view) {
-                int position = getAdapterPosition();
-                HomeText homeText = hometext[position];
-
-                Intent intent = new Intent(HomeActivity.this, HomeActivity.class);
-                intent.putExtra("title", homeText.txtFood);
-                intent.putExtra("price", homeText.txtPrice);
-
-                startActivity(intent);
-            }
-        }
-    }
 
 
-}
+
+
+
+
---
+++ app/src/main/java/kh/edu/app/myproject/lc_luncher/HomeAdapter.java
@@ -0,0 +1,66 @@
+package kh.edu.app.myproject.lc_luncher;
+
+import android.app.LauncherActivity;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import kh.edu.app.myproject.lc_luncher.DataModel.HomeText;
+
+/**
+ * Created by Mork Pin on 6/6/2017.
+ */
+
+
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
+    private List<HomeText> homeTexts;
+
+    public HomeAdapter(List<HomeText> homeTexts) {
+        this.homeTexts = homeTexts;
+    }
+
+    @Override
+    public HomeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_home,parent,false);
+        ViewHolder viewHolder=new ViewHolder(v);// return new ViewHolder(v);
+        return viewHolder;
+
+    }
+
+
+    @Override
+    public void onBindViewHolder(HomeAdapter.ViewHolder holder, int position) {
+        HomeText homeText=homeTexts.get(position);
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return homeTexts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView txt_food;
+        public TextView txt_price;
+        public ImageView img_food;
+        public ImageView img_black;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            txt_food=(TextView)itemView.findViewById(R.id.txt_food);
+            txt_price=(TextView)itemView.findViewById(R.id.txt_price);
+            img_food= (ImageView) itemView.findViewById(R.id.img_food);
+            img_black= (ImageView) itemView.findViewById(R.id.img_black);
+
+        }
+    }
+}
